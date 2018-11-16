feea376e33c1361fef347288f6740db338eeb4a6
shaimaaalaqtash <elias25112@yahoo.com>
2018-11-17 00:47:42 +0300

    change

--- Python/week6.py
+++ Python/week6.py
@@ -5,8 +5,9 @@ grade_one= {'Sami': [19, 18, 19.5, 30, 10], 'Ahmad': [15, 14, 16, 21, 7], 'Faris
 grade_two= {'Lana': [17, 19, 20, 28, 9], 'Dina': [18.5, 19.5, 20, 29, 10], 'Maha': [20, 20, 18, 26, 9], 'Abeer': [16, 18, 19.5, 25, 8]}
 
 grade_three= {'Rima': [18, 19, 18, 26, 9], 'Tala': [20, 20, 19, 29, 10], 'Lamar': [19, 20, 18, 26, 9], 'Rola': [15, 14, 16, 19, 7], 'Naya': [9, 6, 11, 14, 7], 'Dalal': [1, 5, 2, 6, 7], 'Ola': [19.5, 20, 20, 29.5, 10]}
-print("Choose one: students_names, student_score, students_count")
+print"Choose one: students_names, student_score, students_count")
 name,grade =input('Enter name:'),input('Enter grade:')
+
 def students_names(grade):
     names=grade.keys()
     return names
@@ -19,7 +20,7 @@ def student_count(grade):
     count=len(grade)
     return count
 a=input('done or more:')
-if a='done':
+if a=='done':
     exit
-elif a='more':
+elif a=='more':
     print("Choose one: students_names, student_score, students_count")
